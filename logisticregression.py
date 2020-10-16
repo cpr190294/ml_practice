76ad395a7d282e677979de268580415fd59e2cbe
cpr190294 <72794588+cpr190294@users.noreply.github.com>
2020-10-16 17:31:37 +0530

    Update logisticregression.py

--- logisticregression.py
+++ logisticregression.py
@@ -46,3 +46,29 @@ y_pred = r.predict(x_test)
 #print(pd.DataFrame({'Actual':y_test,'prection' : y_pred}))
 from sklearn.metrics import precision_score
 print(precision_score(y_test, y_pred))
+
+#Logistic Regression 2nd example
+import seaborn as sb
+import pandas as pd
+import numpy as np
+import sklearn.linear_model as ln
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+can = {'gmat': [780,750,690,710,680,730,690,720,740,690,610,690,710,680,770,610,580,650,540,590,620,600,550,550,570,670,660,580,650,660,640,620,660,660,680,650,670,580,590,690],
+              'gpa': [4,3.9,3.3,3.7,3.9,3.7,2.3,3.3,3.3,1.7,2.7,3.7,3.7,3.3,3.3,3,2.7,3.7,2.7,2.3,3.3,2,2.3,2.7,3,3.3,3.7,2.3,3.7,3.3,3,2.7,4,3.3,3.3,2.3,2.7,3.3,1.7,3.7],
+              'work_experience': [3,4,3,5,4,6,1,4,5,1,3,5,6,4,3,1,4,6,2,3,2,1,4,1,2,6,4,2,6,5,1,2,4,6,5,1,2,1,4,5],
+              'admitted': [1,1,0,1,0,1,0,1,1,0,0,1,1,0,1,0,0,1,0,0,1,0,0,0,0,1,1,0,1,1,0,0,1,1,1,0,0,0,0,1]
+              }
+
+df = pd.DataFrame(can)
+df.head()
+x = df.iloc[:,:-1]
+y = df.iloc[:,3]
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+r = ln.LogisticRegression()
+r.fit(x_train,y_train)
+y_pred = r.predict(np.array([770,3.4,3]).reshape(1,-1))
+#print(pd.DataFrame({'Actual :':y_test,'Prediction :':y_pred}))
+print(y_pred)
+print(np.array([[780,4,3]]))
